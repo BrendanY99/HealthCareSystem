d1cb56dc8cf9c9503c35bb8760f9ec3c51478f47
Brendan Yumnam <36328001+BranYummy@users.noreply.github.com>
2021-09-30 14:01:40 +0530

    Initial Commit

---
+++ hcsapi/src/main/java/com/sprint1/hcsapi/HcsapiApplication.java
@@ -0,0 +1,13 @@
+package com.sprint1.hcsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcsapiApplication.class, args);
+	}
+
+}
